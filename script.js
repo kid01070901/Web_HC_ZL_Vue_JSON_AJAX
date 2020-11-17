94a9bbad77dc1114cc34b1bbcb3df1ca4756e0a6
kid01070901 <snopy1734@gmail.com>
2020-11-17 19:58:28 +0800

    添加AJAX傳送要求

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ new Vue({
     el: "#content",
     data: {
         datas: null
+    },
+    // 所有元素顯示後執行一次
+    mounted() {
+        // 新增 XML 物件
+        var xhr = new XMLHttpRequest();
+        // 開啟並指定 API 網址
+        xhr.open("GET", "https://data.tycg.gov.tw/api/v1/rest/datastore/a7db5f95-ac1c-4597-8bf5-c5be0e57ae5f?format=json");
+        // 傳送要求
+        xhr.send();
     }
 });
 
